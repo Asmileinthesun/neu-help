0263280a7844373d215974e68961fd5d1848780c
yangguangshenhaoweifengbuzao <310873984@qq.com>
2022-09-13 22:47:03 +0800

    第十三天（9.13）新增商品(85)

--- product/src/main/java/com/hzx/product/controller/CategoryBrandRelationController.java
+++ product/src/main/java/com/hzx/product/controller/CategoryBrandRelationController.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hzx.product.entity.BrandEntity;
+import com.hzx.product.vo.BrandVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,6 +32,18 @@ public class CategoryBrandRelationController {
     @Autowired
     private CategoryBrandRelationService categoryBrandRelationService;
 
+//    /product/categorybrandrelation/brands/list
+    @GetMapping("brands/list")
+    public R RelationBrandslist(@RequestParam("catId") Long catId){
+       List<BrandEntity>vos= categoryBrandRelationService.getBrandsByCatId(catId);
+        List<BrandVo> list = vos.stream().map(item -> {
+            BrandVo brandVo = new BrandVo();
+            brandVo.setBrandId(item.getBrandId());
+            brandVo.setBrandName(item.getName());
+            return brandVo;
+        }).collect(Collectors.toList());
+        return R.ok().put("data", list);
+    }
     /**
      * 列表
      */
--- product/src/main/java/com/hzx/product/service/impl/CategoryBrandRelationServiceImpl.java
+++ product/src/main/java/com/hzx/product/service/impl/CategoryBrandRelationServiceImpl.java
@@ -5,9 +5,14 @@ import com.hzx.product.dao.BrandDao;
 import com.hzx.product.dao.CategoryDao;
 import com.hzx.product.entity.BrandEntity;
 import com.hzx.product.entity.CategoryEntity;
+import com.hzx.product.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -26,6 +31,12 @@ public class CategoryBrandRelationServiceImpl extends ServiceImpl<CategoryBrandR
     CategoryDao categoryDao;
     @Autowired
     BrandDao brandDao;
+
+    @Autowired
+    BrandService brandService;
+
+    @Autowired
+    CategoryBrandRelationDao categoryBrandRelationDao;
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryBrandRelationEntity> page = this.page(
@@ -60,6 +71,17 @@ public class CategoryBrandRelationServiceImpl extends ServiceImpl<CategoryBrandR
         //第二种方法
         this.baseMapper.updateCategory(catId,name);
         System.err.println("catId = " + catId);
+    }
+
+    @Override
+    public List<BrandEntity> getBrandsByCatId(Long catId) {
+        List<CategoryBrandRelationEntity> list = categoryBrandRelationDao.selectList(new QueryWrapper<CategoryBrandRelationEntity>().eq("catelog_id", catId));
+        List<BrandEntity> collect = list.stream().map(item -> {
+            Long brandId = item.getBrandId();
+            BrandEntity byId = brandService.getById(brandId);
+            return byId;
+        }).collect(Collectors.toList());
+        return collect;
     }
 
 }
