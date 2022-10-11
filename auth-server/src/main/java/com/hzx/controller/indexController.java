09d42bd46457755f422e71fcfa17bb539a63beef
yangguangshenhaoweifengbuzao <310873984@qq.com>
2022-10-11 17:29:13 +0800

    （10.11）认证服务(216)（跳过186-192,207-209,213-214）(验证码没有完成)

--- auth-server/src/main/java/com/hzx/controller/indexController.java
+++ auth-server/src/main/java/com/hzx/controller/indexController.java
@@ -1,17 +1,33 @@
 package com.hzx.controller;
 
+import com.hzx.vo.UserRegister;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-//@Controller
+import javax.naming.Binding;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Controller
 public class indexController {
-//    @GetMapping("login.html")
-//    public String loginPage(){
-//        return "login";
-//    }
-//
-//    @GetMapping("reg.html")
-//    public String regPage(){
-//        return "reg";
-//    }
+    @PostMapping("/regist")
+    public String regist(@Valid UserRegister userRegister, BindingResult result,
+                         RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            Map<String, String> collect = result.getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, fieldError -> fieldError.getDefaultMessage()));
+//            model.addAttribute("errors",collect);
+            redirectAttributes.addFlashAttribute("errors",collect);
+            return "redirect:http://auth.gulimall.com/reg.html";
+
+        }
+        return "redirect:/login.html";
+    }
 }
