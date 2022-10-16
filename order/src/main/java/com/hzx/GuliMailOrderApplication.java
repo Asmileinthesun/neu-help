99920f4dea87ad6f9dfd6b41ff6f58730e28098a
yangguangshenhaoweifengbuzao <310873984@qq.com>
2022-10-16 20:18:20 +0800

    （10.11）订单服务(264)（跳过186-192,207-209,213-214,232-235）(验证码没有完成)

--- order/src/main/java/com/hzx/GuliMailOrderApplication.java
+++ order/src/main/java/com/hzx/GuliMailOrderApplication.java
@@ -3,6 +3,9 @@ package com.hzx;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
 @EnableRabbit
 //@MapperScan("com.hzx.product.dao")
 @SpringBootApplication
